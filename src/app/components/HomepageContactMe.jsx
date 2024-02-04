a8a34db8f99ad2a72357c04f81973caa1358d24e
Sakib Ahamed Shahon <53832890+sakib3201@users.noreply.github.com>
2024-02-05 00:36:52 +0600

    Added contact me section

---
+++ src/app/components/HomepageContactMe.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { SiGithub, SiLinkedin, SiYoutube } from "react-icons/si";
+
+const HomepageContactMe = () => {
+  return (
+    <div>
+      <div id="Projects" className="flex justify-center items-center mt-10 mb-5 text-5xl font-montseratt font-bold">
+        Let's Connect!
+      </div>
+      <div className="flex justify-center">
+        <div className="flex self-center flex-col md:flex-row shadow-lg rounded-lg p-4 w-3/5 bg-primary m-2">
+          <div className="md:w-1/3 flex justify-center items-center"> 
+            <img
+              src="/images/professional.webp"
+              alt="Profile"
+              className="object-cover rounded-lg bg-neutral-100 mask mask-squircle"
+            />
+          </div>
+          <div className="md:w-2/3 mt-4 md:mt-0 md:ml-4 flex flex-col justify-center">
+            <form>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-200 font-bold mb-2"
+                >
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full px-3 py-2 bg-slate-600 border rounded-lg focus:outline-none focus:border-secondary"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="block text-gray-200 font-bold mb-2"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  className="w-full bg-slate-600 px-3 py-2 border rounded-lg focus:outline-none focus:border-secondary"
+                  rows="5"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="bg-secondary hover:bg-accent text-black px-4 py-2 rounded-lg"
+              >
+                Send
+              </button>
+            </form>
+            <div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomepageContactMe;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import ProjectSlider from "../app/components/ProjectSlider";
 import ServiceSection from "../app/components/ServiceSection";
 import Footer from "../app/components/Footer";
 import HomepageTimeLine from "../app/components/HomepageTimeLine";
+import HomepageContactMe from "../app/components/HomepageContactMe"; 
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <ProjectSlider></ProjectSlider>
       <ServiceSection></ServiceSection>
       <HomepageTimeLine></HomepageTimeLine>
+      <HomepageContactMe></HomepageContactMe>
       <Footer></Footer>
     </main>
   );
