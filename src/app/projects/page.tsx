3678825e88c0aafc3ecd1f3309e3d636aecd863f
Sakib Ahamed Shahon <53832890+sakib3201@users.noreply.github.com>
2024-02-18 01:33:53 +0600

    Created files for projects displaying page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import LegacyNavbar from "../../app/components/LegacyNavbar";
+import Footer from "../../app/components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <LegacyNavbar></LegacyNavbar>
+      <Footer></Footer>
+    </main>
+  );
+}
